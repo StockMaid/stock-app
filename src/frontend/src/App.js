2c2973dec95284f0f14726d06e0ca5ea92cb923e
Shanesreal <Shanesreal@gmail.com>
2021-09-04 19:27:46 -0400

    add navbar and scroll to App.js

--- src/frontend/src/App.js
+++ src/frontend/src/App.js
@@ -4,11 +4,15 @@ import { ManufacturerPage } from "./pages/ManufacturerPage";
 import { DatePage } from "./pages/DatePage";
 import { SkuPage } from "./pages/SkuPage";
 import { HomePage } from "./pages/HomePage";
+import Navbar from "./navbar/Navbar";
+import ScrollToTop from "./components/ScrollToTop";
 
 function App() {
     return (
         <div className="App">
             <Router>
+                <ScrollToTop />
+                <Navbar />
                 <Switch>
                     <Route path="/skus/:sku">
                         <SkuPage />
