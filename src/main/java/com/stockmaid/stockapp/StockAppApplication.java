d3dfece828999e4b6909d0a0b0282d4cf228243b
Shanesreal <Shanesreal@gmail.com>
2021-09-04 17:23:02 -0400

    Initial commit

---
+++ src/main/java/com/stockmaid/stockapp/StockAppApplication.java
@@ -0,0 +1,13 @@
+package com.stockmaid.stockapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StockAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StockAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/stockmaid/stockapp/StockAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.stockmaid.stockapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StockAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
